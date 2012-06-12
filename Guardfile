b202adbed103f7f859b319f577c1372418556969
keppy <feralmode@gmail.com>
2012-06-12 10:50:32 -0700

    static pages up

--- Guardfile
+++ Guardfile
@@ -38,6 +38,5 @@ guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, :rspec_env => { 'RAIL
   watch('Gemfile')
   watch('Gemfile.lock')
   watch('spec/spec_helper.rb')
-  watch('test/test_helper.rb')
   watch('spec/support/')
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Debateable')
     end
+    
+    it "should have the correct title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Debateable | Home")
+
+    end
   end
 
   describe "About page" do
@@ -16,13 +23,26 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_content('About')
     end
+
+    it "should have the correct title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Debateable | About")
+    end  
+
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/about'
+      visit '/static_pages/help'
       page.should have_content('Help')
+    end
+
+    it "should have the correct title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Debateable | Help")
     end
   end
 end

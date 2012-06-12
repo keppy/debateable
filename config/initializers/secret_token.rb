71e825d3bab01b1de8de96c9ce0ca34786a9e075
keppy <feralmode@gmail.com>
2012-06-12 01:17:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debateable::Application.config.secret_token = '4e2e1a10aeaf72a9cb066aa812d671178cea1f784728a209273a3380ec743cd7f9151648decac4cb270efdc2ddfa132e28dc4e04f1eaa5f4dbfef5bea87da2f8'

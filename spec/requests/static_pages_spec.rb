c29c90ba04ffd2c6fa4088ba4883164b43d668be
keppy <feralmode@gmail.com>
2012-06-13 14:38:46 -0700

    refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,28 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject {page}  
   
   describe "Home page" do
-
-    it "should have the content 'Debateable'" do
-      visit '/static_pages/home'
-      page.should have_content('Debateable')
-    end
+    before { visit root_path }
+    
+      it { should have_selector('h1',    text: 'Welcome to debateable.') }
+      it { should have_selector('title', text: full_title('')) }
+      it { should_not have_selector 'title', text: '| Home' }  
+  end
    
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                                :text => "Debateable")
-
-    end
- 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-
-    end
-  end
-
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
-    end
-
-    it "should have the correct title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                                :text => "Debateable | About")
-    end  
-
-  end
+      it { should have_selector('h1', text: 'About debateable') }
+      it { should have_selector('title', text: full_title('About')) }
+  end  
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
-
-    it "should have the correct title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                                :text => "Debateable | Help")
-    end
+      it { should have_selector('h1', text: 'Help') }
+      it { should have_selector('title', text: full_title('Help')) }
   end
 end

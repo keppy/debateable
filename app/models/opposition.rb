c4d94e729a2cb9d16e8ed65937e21d268307e288
James <feralmode@gmail.com>
2012-07-06 13:01:11 -0700

    fixed?

--- app/models/opposition.rb
+++ app/models/opposition.rb
@@ -1,9 +1,5 @@
 class Opposition < ActiveRecord::Base
   attr_accessible :footnotes, :response, :slide, :title
 
-<<<<<<< HEAD
   belongs_to :user, :inverse_of => :oppositions
-=======
-  belongs_to :debate
->>>>>>> e0a5ffe67c1b2224edab6041c32406f187b8191d
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,9 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-<<<<<<< HEAD
   
-  has_many :propositions, :inverse_of => :user
   has_many :oppositions, :inverse_of => :user  
-=======
-
-  has_many :debates 
-  has_many :propositions, :through => :debates
-  has_many :oppositions, :through => :debates
->>>>>>> e0a5ffe67c1b2224edab6041c32406f187b8191d
-
+  has_many :propositions, :inverse_of => :user
   before_save { |user| user.email = email.downcase }
 
   validates :email, presence:   true, 
